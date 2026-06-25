31eaef829f4274e394e4bb26e2dcdf1597998a30
SUJIN J <sjnnus@gmail.com>
2026-06-25 16:36:09 +0900

    map() 동적 렌더링 및 Contents 컴포넌트 분리, 이미지 경로 public으로 이동

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import './App.css';
-import dog_covy from './img/covy.jpg'
-import dog_bbomi from './img/bbomi.png'
-import dog_dal from './img/dal.png'
+// import dog_covy from './img/covy.jpg'
+// import dog_bbomi from './img/bbomi.png'
+// import dog_dal from './img/dal.png'
 import { useState } from 'react';
 
 import data from './data.js';
 // import 작명 from './data.js';
 
 function App() {
-  
+  // 사용할 데이터
   let [dogs] = useState(data);
 
   return (
     <div className="App">
+      {/* 상단바 영역 */}
       <Navbar bg="dark" data-bs-theme="dark">
         <Container>
           <Navbar.Brand href="#home" className='navBar'>견짱시대™</Navbar.Brand>
@@ -25,29 +26,34 @@ function App() {
         </Container>
       </Navbar>
 
+      {/* 콘텐츠 영역 */}
       <div className='container'>
         <div className='row'>
-          <div className='col-md-4'>
-            <img src={dog_covy} className='cute-img' />
-            <h4> { dogs[0].name } </h4>
-            <p> { dogs[0].age }세</p>
-          </div>
-          <div className='col-md-4'>
-            <img src={dog_bbomi} className='cute-img' />
-            <h4>뽀미</h4>
-            <p>5세</p>
-          </div>
-          <div className='col-md-4'>
-            <img src={dog_dal} className='cute-img' />
-            <h4>박달</h4>
-            <p>15세</p>
-          </div>
+          {
+            dogs.map(function(list, i) {
+              return (
+                <Contents dogs={dogs} i={i} key={i}/>
+              )
+            })
+          }
         </div>
-
       </div>
+
+      
 
     </div>
   );
+}
+
+/* 컴포넌트 영역 */
+function Contents(props) {
+  return (
+    <div className='col-md-4'>
+      <img src={ props.dogs[props.i].img } className='cute-img' />
+      <h4> { props.dogs[props.i].name } </h4>
+      <p> { props.dogs[props.i].age }세</p>
+    </div>
+  )
 }
 
 export default App;
--- src/data.js
+++ src/data.js
@@ -3,21 +3,24 @@ let data = [
     id : 0,
     name : "임코비",
     age : 4,
-    manager : "임희진"
+    manager : "임희진",
+    img : process.env.PUBLIC_URL + "/img/covy.jpg"
   },
 
   {
     id : 1,
     name : "뽀미",
     age : 5,
-    manager : "밥영돈"
+    manager : "밥영돈",
+    img : process.env.PUBLIC_URL + "/img/bbomi.png"
   },
 
   {
     id : 2,
     name : "박달",
     age : 15,
-    manager : "Mademoiselle Park"
+    manager : "Mademoiselle Park",
+    img : process.env.PUBLIC_URL + "/img/dal.png"
   }
 ]
 
