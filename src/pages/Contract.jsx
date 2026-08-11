d8162b69ec418a392230abc5c2061750275be74c
SUJIN J <sjnnus@gmail.com>
2026-08-11 09:22:44 +0900

    Redux store에 슬라이스 추가 및 페이지 연결(데이터 표시)

--- src/pages/Contract.jsx
+++ src/pages/Contract.jsx
@@ -1,24 +1,40 @@
 import { Table } from "react-bootstrap";
+import { useSelector } from "react-redux";
 
 function Contract() {
+
+    let state = useSelector((state)=>{ return state.wishlist });
+
     return (
         <div>
+            <h3>잡아야 한다</h3>
             <Table>
                 <thead>
                     <tr>
-                    <th>#</th>
+                    <th>No.</th>
                     <th>선수명</th>
-                    <th>계약</th>
-                    <th>변경</th>
+                    <th>포지션</th>
+                    <th>FA</th>
+                    <th>연봉</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr>
-                    <td>1</td>
-                    <td>안녕</td>
-                    <td>안녕</td>
-                    <td>안녕</td>
-                    </tr>
+                    {/* <td>{ state[0].id }</td>
+                    <td>{ state[0].name }</td>
+                    <td>{ state[0].position }</td> */}
+                    {
+                        state.map(function(state, i) {
+                            return (
+                                <tr>
+                                    <td>{ state.id }</td>
+                                    <td>{ state.name }</td>
+                                    <td>{ state.position }</td>
+                                    <td>{ state.fa }</td>
+                                    <td>{ state.salary } 원</td>
+                                </tr>
+                            )
+                        })
+                    }
                 </tbody>
                 </Table> 
         </div>
--- src/store.js
+++ src/store.js
@@ -1,5 +1,27 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+let user = createSlice({
+    name: 'user',
+    initialState: 'tigers'
+})
+
+let year = createSlice({
+    name: 'year',
+    initialState: [3, 4, 5]
+})
+
+let wishlist = createSlice({
+    name: 'wishlist',
+    initialState: [
+        {id: 3, name: '김호령', position: '외야수', fa: '26시즌 이후', salary: '2억 5,000만'},
+        {id: 5, name: '김태군', position: '포수', fa: '26시즌 이후', salary: '6억'}
+    ]
+})
 
 export default configureStore({
-  reducer: { }
+  reducer: { 
+    user        : user.reducer,
+    year        : year.reducer,
+    wishlist    : wishlist.reducer
+   }
 })
